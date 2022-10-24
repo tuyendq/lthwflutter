582698961492a4628f51ee99a20d543dd2ec23a0
Tuyen Dang <tuyendq@gmail.com>
2022-10-24 22:55:11 +0700

    try english_words

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'iPractice',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('iPractice'),
         ),
-        body: const Center(
-          child: Text('Learn . Unlearn . Relearn'),
+        body: Center(
+          // child: Text('Learn . Unlearn . Relearn'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
