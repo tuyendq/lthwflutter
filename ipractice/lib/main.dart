f8fe8e15fad18eeee0506dbc6e463a3d4a671d72
Tuyen Dang <tuyendq@gmail.com>
2022-10-25 23:21:59 +0700

    add ipractice/ folder

---
+++ ipractice/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        '"Everything is practice." — Pelé',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
