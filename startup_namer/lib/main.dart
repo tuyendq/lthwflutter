f2a05b223b2675d94207a05fceed7f5b69dbde60
Tuyen Dang <tuyendq@gmail.com>
2022-10-25 22:59:13 +0700

    6. Navigate to a new screen

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -93,5 +93,31 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 
-  void _pushSaved() {}
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(title: const Text('Saved Suggestions')),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
 }
