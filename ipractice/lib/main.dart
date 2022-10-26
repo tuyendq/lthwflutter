c5f68fb22e39a27840cd7d39ce41335c7369151b
Tuyen Dang <tuyendq@gmail.com>
2022-10-26 16:31:10 +0700

    Refactor: extract to MyApp class

--- ipractice/lib/main.dart
+++ ipractice/lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const Center(
+  runApp(const MyApp()
+      // const Center(
+      //   child: Text(
+      //     '"Everything is practice." — Pelé',
+      //     textDirection: TextDirection.ltr,
+      //   ),
+      // ),
+      );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
       child: Text(
         '"Everything is practice." — Pelé',
         textDirection: TextDirection.ltr,
       ),
-    ),
-  );
+    );
+  }
 }
