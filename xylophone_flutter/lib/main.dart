c4d8eb4d2243e6a09e362f07ba89c5bcbf612913
Tuyen Dang <tuyendq@gmail.com>
2022-11-06 12:59:19 +0700

    add 7 notes and add argument to playSound() method

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -4,9 +4,9 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playSound() {
+  void playSound(int noteNumber) {
     final player = AudioPlayer();
-    player.play(AssetSource('note1.wav'));
+    player.play(AssetSource('note$noteNumber.wav'));
   }
 
   @override
@@ -18,15 +18,67 @@ class XylophoneApp extends StatelessWidget {
             children: [
               TextButton(
                 onPressed: () {
-                  playSound();
-                  // print('button pressed');
+                  playSound(1);
                 },
                 child: Container(
-                  height: 20.0,
+                  height: 30.0,
                   color: Colors.red,
                 ),
               ),
-              // Container(),
+              TextButton(
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Container(
+                  height: 30.0,
+                  color: Colors.orange,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Container(
+                  height: 30.0,
+                  color: Colors.yellow,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Container(
+                  height: 30.0,
+                  color: Colors.green,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Container(
+                  height: 30.0,
+                  color: Colors.teal,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Container(
+                  height: 30.0,
+                  color: Colors.blue,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Container(
+                  height: 30.0,
+                  color: Colors.purple,
+                ),
+              ),
             ],
           ),
         ),
