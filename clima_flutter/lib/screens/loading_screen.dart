0256a8045a513e55f48ecf2b9699a190d68e7d23
Tuyen Dang <tuyendq@gmail.com>
2022-11-16 22:57:58 +0700

    extract Location class into services/location.dart file

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -15,21 +15,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-      print(position);
-      // somethingThatExpectsLessThan10(12);
-    } catch (e) {
-      print(e);
-    }
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
   }
-
-  // void somethingThatExpectsLessThan10(int n) {
-  //   if (n > 10) {
-  //     throw 'n is greater than 10, n should always be less than 10.';
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
