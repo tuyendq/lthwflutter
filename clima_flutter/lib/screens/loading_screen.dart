a9be3c2ec13649837052f28a585acf746f11b697
Tuyen Dang <tuyendq@gmail.com>
2022-11-18 20:45:30 +0700

    refactor: networking.dart

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
+import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 const apiKey = '354461a08029560d19b6e965d0584c9a';
 
@@ -20,69 +20,39 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     print('initState Called.');
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
 
     latitude = location.latitude;
     longitude = location.longitude;
-    print('Longitude: $longitude');
-    print('Latitude:$latitude');
-    getData();
-  }
+    // print('Longitude: $longitude');
+    // print('Latitude:$latitude');
 
-  void getData() async {
-    http.Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'));
-    if (response.statusCode == 200) {
-      String data = response.body;
-      print(data);
-      var decodedData = jsonDecode(data);
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-      double longitude = decodedData['coord']['lon'];
-      print(longitude);
+    var weatherData = await networkHelper.getData();
 
-      String weatherDescription = decodedData['weather'][0]['description'];
-      print(weatherDescription);
-
-      double temperature = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
-      print('Temperature: $temperature');
-      print(condition);
-      print('CityName: $cityName');
-    } else {
-      print(response.statusCode);
-    }
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    // String myMargin = '15';
-    String myMargin = 'acb';
-    double myMarginAsDouble;
-    try {
-      myMarginAsDouble = double.parse(myMargin);
-    } catch (e) {
-      print(e);
-    }
-
     return Scaffold(
-      // body: Center(
-      //   child: ElevatedButton(
-      //     onPressed: () {
-      //       //Get the current location
-      //       getLocation();
-      //     },
-      //     child: Text('Get Location'),
-      //   ),
-      // ),
-      body: Container(
-        margin: EdgeInsets.all(myMarginAsDouble ?? 30),
-        color: Colors.red,
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+            getLocationData();
+          },
+          child: Text('Get Location'),
+        ),
       ),
     );
   }
